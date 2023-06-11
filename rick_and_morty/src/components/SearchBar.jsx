6139937925253173c23358a0d63c7bae66d660c5
momel-jgo <melliebuitrago@gmail.com>
2023-06-10 21:37:49 -0500

    src/App.css

---
+++ rick_and_morty/src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+
+function SearchBar({ onSearch }) {
+   return (
+      <div>
+         <input type='search' />
+         <button onClick={onSearch}>Agregar</button>
+      </div>
+   );
+}
+
+export default SearchBar;
