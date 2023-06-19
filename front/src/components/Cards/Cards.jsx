01b2a2606b5d064e638b88857958fabbe1fdc230
momel-jgo <melliebuitrago@gmail.com>
2023-06-19 14:34:33 -0500

    switch folder to front

---
+++ front/src/components/Cards/Cards.jsx
@@ -0,0 +1,24 @@
+import Card from '../Card/Card';
+
+function Cards({ characters, onClose}) {
+
+   return (
+         <div>
+               {
+                  characters.map(({id, name, species, gender, image}) => {
+                     return <Card
+                        key={id}
+                        id={id}
+                        name ={name}
+                        species={species}
+                        gender={gender}
+                        image={image}
+                        onClose={() => onClose(id)}
+                     />
+                  })
+               }
+         </div>
+   )
+};
+
+export default Cards;
